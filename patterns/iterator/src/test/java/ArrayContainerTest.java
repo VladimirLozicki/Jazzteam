d1160e5fffa18234e393d35ca0331d3724dde50b
vladimirlozickiy <volodya199996@gmail.com>
2019-04-26 15:32:04 +0300

    before demo1

--- patterns/iterator/src/test/java/ArrayContainerTest.java
+++ patterns/iterator/src/test/java/ArrayContainerTest.java
@@ -6,19 +6,20 @@ public class ArrayContainerTest {
     Iterator iterator = arrayContainer.getIterator();
 
 
-    @Test
-    public void testOneIteration() {
-        iterator.next();
-        assertEquals(iterator.next(), "1");
-    }
+//    @Test
+//    public void testOneIteration() {
+//        iterator.next();
+//        assertEquals(iterator.next(), "1");
+//    }
+//
+//    @Test
+//    public void testTwoIteration() {
+//        iterator.next();
+//        iterator.next();
+//        assertEquals(iterator.next(), "2");
+//    }
 
-    @Test
-    public void testTwoIteration() {
-        iterator.next();
-        iterator.next();
-        assertEquals(iterator.next(), "2");
-    }
-
+    
     @Test
     public void testArray() {
         assertNotNull(arrayContainer.array.length);
